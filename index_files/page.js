86f02b7f91784a6a97580244b068df063e5fe933
Eduard-Simonyan-test <esimonyan2014@gmail.com>
2017-11-27 02:03:56 +0400

    fixed menu, some js animation

--- index_files/page.js
+++ index_files/page.js
@@ -19,7 +19,7 @@ wow.init();
 /*====================================================*/
 $(window).load(function(){
 
-    $('.main-nav li a').bind('click',function(event){
+    $('.main-nav li a, header a, .knowMore').bind('click',function(event){
         var $anchor = $(this);
 
         $('html, body').stop().animate({
